d4793f7f9105dfb18fe4f52aa3ddf47b295d534a
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-06 21:11:51 +0100

    Registartion with custom user model

--- account/backends.py
+++ account/backends.py
@@ -17,7 +17,6 @@ class CustomUserModelBackend(ModelBackend):
             )
         except UserModel.DoesNotExist:
             # Run the default password hasher once to reduce the timing
-            # difference between an existing and a nonexistent user (#20760).
             UserModel().set_password(password)
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
