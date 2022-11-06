107c7e16f8b1b3283c69756df2bdb8353384d946
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-06 16:01:16 +0100

    Handling user log-in and log-out

--- weather_manager/settings.py
+++ weather_manager/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     'weather.apps.WeatherConfig',
+    # 3rd party
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
