5ee4c57539e8c9175446e42c170c410ef49dbae1
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-08 12:44:01 +0100

    Urls, view, and templates for the current weather

--- weather/urls.py
+++ weather/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, current_weather
 
 
 urlpatterns = [
-    path("", home_page_view, name="home")
+    path("", home_page_view, name="home"),
+    path('current-weather/', current_weather, name="current_weather"),
+
 ]
--- weather/views.py
+++ weather/views.py
@@ -22,3 +22,16 @@ def home_page_view(request):
             "forecast_default_weather": response_forecast.json(),
         },
     )
+
+def current_weather(request):
+    query = request.GET.get('weather_query')
+    current_weather_api_url = f"https://api.weatherapi.com/v1/current.json"
+    current_weather_api_url += f"?key={wak}&q={query}&aqi=no"
+    forecast_weather_api_url = f"https://api.weatherapi.com/v1/forecast.json"
+    forecast_weather_api_url += f"?key={wak}&q={query}&aqi=no"
+    response_current = requests.get(current_weather_api_url)
+    response_forecast = requests.get(forecast_weather_api_url)
+    return render(request, 'weather/current-weather.html',
+                  {'current_weather_data': response_current.json(),
+                   'forecast_current_weather': response_forecast.json(),
+                   'query': query})
