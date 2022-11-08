16610a12ee75755f2cc6341043e198366a9de08f
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-08 21:20:48 +0100

    Prepare forecast and historical information

--- weather/urls.py
+++ weather/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home_page_view, current_weather
+from .views import home_page_view, current_weather, forecast_weather, historical_weather
 
 
 urlpatterns = [
     path("", home_page_view, name="home"),
     path('current-weather/', current_weather, name="current_weather"),
+    path('forecast-weather/', forecast_weather, name="forecast_weather"),
+    path('historical-weather/', historical_weather, name="historical_weather"),
 
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,7 @@
 import requests
+
 from django.conf import settings as conf_settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 wak = conf_settings.WEATHER_API_KEY
@@ -9,29 +11,43 @@ def home_page_view(request):
     default_location = conf_settings.DEFAULT_LOCATION
     current_weather_api_url = f"https://api.weatherapi.com/v1/current.json"
     current_weather_api_url += f"?key={wak}&q={default_location}&aqi=no"
-    forecast_weather_api_url = f"https://api.weatherapi.com/v1/forecast.json"
-    forecast_weather_api_url += f"?key={wak}&q={default_location}&aqi=no"
     response_current = requests.get(current_weather_api_url)
-    response_forecast = requests.get(forecast_weather_api_url)
     return render(
         request,
         "home.html",
         {
             "default_weather_data": response_current.json(),
             "default_location": default_location,
-            "forecast_default_weather": response_forecast.json(),
-        },
+        }
     )
 
 def current_weather(request):
     query = request.GET.get('weather_query')
     current_weather_api_url = f"https://api.weatherapi.com/v1/current.json"
     current_weather_api_url += f"?key={wak}&q={query}&aqi=no"
-    forecast_weather_api_url = f"https://api.weatherapi.com/v1/forecast.json"
-    forecast_weather_api_url += f"?key={wak}&q={query}&aqi=no"
     response_current = requests.get(current_weather_api_url)
-    response_forecast = requests.get(forecast_weather_api_url)
     return render(request, 'weather/current-weather.html',
                   {'current_weather_data': response_current.json(),
-                   'forecast_current_weather': response_forecast.json(),
+                   'query': query})
+
+
+@login_required
+def forecast_weather(request):
+    query = request.GET.get('weather_query')
+    forecast_weather_api_url = f"https://api.weatherapi.com/v1/forecast.json"
+    forecast_weather_api_url += f"?key={wak}&q={query}&days=2&aqi=no&alerts=no"
+    response_forecast = requests.get(forecast_weather_api_url)
+    return render(request, 'weather/forecast-weather.html',
+                  {'forecast_weather': response_forecast.json(),
+                   'query': query})
+
+@login_required
+def historical_weather(request):
+    query = request.GET.get('weather_query')
+    historical_weather_api_url = f"http://api.weatherapi.com/v1/history.json"
+    # dobrać się do 5 ostatnich dni i je zapisać w JSONie!!!! 
+    historical_weather_api_url += f"?key={wak}&q={query}&dt=2022-11-06"
+    response_historical = requests.get(historical_weather_api_url)
+    return render(request, 'weather/historical-weather.html',
+                  {'forecast_weather': response_historical.json(),
                    'query': query})
