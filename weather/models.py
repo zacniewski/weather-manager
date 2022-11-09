aba677169a1f249af856254802e9e6b9fc308142
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-09 20:37:10 +0100

    Preparing models

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+class FavouriteLocation(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length = 100)
+
+
+class WeatherData(models.Model):
+    pass
