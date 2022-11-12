2694876c5a466affbeece6434557e9cd7b6b6083
Artur Zacniewski <a.zacniewski@gmail.com>
2022-11-12 23:19:27 +0100

    Models for weathe data

--- weather/models.py
+++ weather/models.py
@@ -10,4 +10,13 @@ class FavouriteLocation(models.Model):
 
 
 class WeatherData(models.Model):
-    pass
+    latitude = models.CharField(max_length=30)
+    longitude = models.CharField(max_length=30)
+    local_time = models.CharField(max_length=30)
+    pressure_mb = models.CharField(max_length=30)
+    wind_kph = models.CharField(max_length=30)
+    wind_dir = models.CharField(max_length=30)
+    timezone = models.CharField(max_length=30)
+    cloudiness = models.CharField(max_length=30)
+    humidity = models.CharField(max_length=30)
+    precipitation = models.CharField(max_length=30)
