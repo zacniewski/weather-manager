e350b90b5cbbf1e8c06fd6e9cb6001b272a213b8
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-13 20:54:47 +0100

    Preparing for Heroku deployment

--- weather_manager/settings.py
+++ weather_manager/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import django_on_heroku
 from decouple import config
 from pathlib import Path
+
+django_on_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
