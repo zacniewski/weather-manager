aa6dcb59b72960ff1f8397511ed6613b886d7406
Artur Zacniewski <artur.zacniewski@stxnext.pl>
2022-11-13 20:44:28 +0100

    Preparing for Heroku deployment

--- weather_manager/settings.py
+++ weather_manager/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS = [
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
